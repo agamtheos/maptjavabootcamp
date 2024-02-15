8ee173bb4db0bf71b3eadac8d6b237eac35afd92
Agam <agamtheos@gmail.com>
2024-02-15 16:29:55 +0700

    feat: add Praktikum1

---
+++ src/main/java/com/bcafinance/testharian/Praktikum1.java
@@ -0,0 +1,34 @@
+package com.bcafinance.testharian;/*
+IntelliJ IDEA 2022.3.1 (Community Edition)
+Build #IC-223.8214.52, built on December 20, 2022
+@Author agam a.k.a. Agam Theos
+Java Developer
+Created on 15/02/2024 15:10
+@Last Modified 15/02/2024 15:10
+Version 1.0
+*/
+
+import java.util.Scanner;
+
+public class Praktikum1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Masukkan Kata : ");
+        String input = scanner.nextLine();
+
+        scanner.close();
+
+        int hasil = 0;
+        for (int i = 0; i < input.length(); i++) {
+            char ch = input.charAt(i);
+            int val = ch;
+
+            if(val < 97) {
+                hasil += val;
+            }
+        }
+
+        System.out.println("Hasil: " + hasil);
+    }
+}
